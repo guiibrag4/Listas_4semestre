96baa01004833b4b4d4b339b98d18f130c2aafe9
Guilherme Braga Rios da Costa <guilhermebragaepic2@gmail.com>
2024-04-30 06:37:52 -0300

    add lista 4

--- lista04/barbearia.py
+++ lista04/barbearia.py
@@ -1,9 +1,49 @@
-# Modele o problema da barbearia com múltiplos barbeiros utilizando threads em 
-# Python. Tenha um número fixo de cadeiras na barbearia e um número variável de 
-# barbeiros. Os clientes chegam à barbearia e verificam se há cadeiras disponíveis. 
-# Se todas as cadeiras estiverem ocupadas, eles saem; caso contrário, eles 
-# ocupam uma cadeira e aguardam serem chamados por um barbeiro. Os 
-# barbeiros, quando disponíveis, chamam o próximo cliente e o atendem. Utilize 
-# semáforos para controlar o acesso às cadeiras e garantir a exclusividade do 
-# atendimento.
+'''
+Modele o problema da barbearia com múltiplos barbeiros utilizando threads em 
+Python. Tenha um número fixo de cadeiras na barbearia e um número variável de 
+barbeiros. Os clientes chegam à barbearia e verificam se há cadeiras disponíveis. 
+Se todas as cadeiras estiverem ocupadas, eles saem; caso contrário, eles 
+ocupam uma cadeira e aguardam serem chamados por um barbeiro. Os 
+barbeiros, quando disponíveis, chamam o próximo cliente e o atendem. Utilize 
+semáforos para controlar o acesso às cadeiras e garantir a exclusividade do 
+atendimento.
+'''
 
+import threading
+import time
+import random
+
+CADEIRAS = 5 # Maiúsculo pq é constante
+barbeiros = 0
+clientes = 10
+
+cadeiras_ocupadas = 0
+cadeiras_livres = threading.Semaphore (CADEIRAS)
+mutex = threading.Lock()
+
+def barbeiro(i):
+    global barbeiros, cadeiras_ocupadas
+    with mutex:
+        barbeiros -= 1
+    print(f'Barbeiro {i} está cortando cabelo de um cliente')
+    time.sleep(random.randint(1, 3))
+    print(f'Barbeiro {i} terminou de cortar o cabelo desse cliente')
+
+def cliente(i):
+    global cadeiras_ocupadas
+    with mutex:
+        if cadeiras_ocupadas < CADEIRAS:
+            cadeiras_ocupadas += 1
+            print(f'Cliente {i} sentou em uma cadeira e está esperando um barbeiro')
+            cadeiras_livres.release()
+        else:
+            print(f'Cliente {i} não encontrou cadeiras disponíveis e foi embora')
+
+print ('Barbearia aberta! Quantos barbeiros terá na sua barbearia?')
+barbeiros = int(input())
+
+for i in range (barbeiros):
+    threading.Thread(target=barbeiro, args=(i,)).start()
+
+for i in range(clientes):
+    threading.Thread(target=cliente, args=(i,)).start()
--- lista04/leitores-escritores.py
+++ lista04/leitores-escritores.py
@@ -7,4 +7,44 @@ acesso ao recurso e garantir que não ocorra condição de corrida entre leitore
 escritores.
 '''
 
+import threading
 
+novaYork_news = 0
+
+# Semáforos
+sem = threading.Semaphore(5) # 5 leitores podem ler ao mesmo tempo
+mutex = threading.Lock() #Semáforo binário, com "down" e "up" (Ações atômicas, que garantem que quando uma operação está sendo executada no semáforo, nenhum processo pode acessar o semáforo)
+
+def leitor(nome):
+    global novaYork_news
+    sem.acquire()
+    print(f'Leitor {nome} está lendo a notícia: {novaYork_news}')
+    sem.release()
+
+def escritor (nome):
+    global novaYork_news
+    mutex.acquire()
+    novaYork_news += 1
+    print (f'Escritor {nome} está escrevendo a notícia: {novaYork_news}')
+    mutex.release()
+    print ()
+    
+threads = []
+for i in range(5):
+    t = threading.Thread(target=leitor, args=(f'{i+1}',))
+    threads.append(t)
+    t.start()
+    
+    t = threading.Thread(target=escritor, args=(f'{i+1}',))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+'''
+acquire() é usado para solicitar o semáforo. Se o valor interno do semáforo for maior que zero, então ele é decrementado e a execução continua. Se o valor interno for zero, acquire() bloqueia e espera até que algum outro thread chame release().
+
+release() é usado para liberar o semáforo. Quando release() é chamado, o valor interno do semáforo é incrementado. Se houver threads esperando para adquirir o semáforo, um deles conseguirá e sua execução continuará.
+
+'''
